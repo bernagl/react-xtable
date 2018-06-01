9de1c389bc20d603e4701b2ff9a1fb44aefced4a
bernagl <luisb.galo@gmail.com>
2018-06-01 12:22:27 -0500

    Fixed import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import './App.css'
+import './rxdt.css'
 
 export default class Datatable extends Component {
   state = {
